8bbc8f110421632c600ab9ec75476554f6cef019
LordCatello <carmine_carratu@libero.it>
2019-12-28 18:49:21 +0100

    Modified the graph class. Probably we have to rewrite it if we want to optimize the algorithm. Added some functions for building a random graph and for testing the correctness of the solution of the algorithm

---
+++ Problem5/test.py
@@ -0,0 +1,49 @@
+from graph import Graph
+from test_functions import is_vertex_cover_correct
+from test_functions import build_random_graph
+
+
+"""
+It tests if the function "is_vertex_cover_correct" is correct.
+
+:return True if the return value of the "is_vertex_cover_correct" is correct.
+        False otherwise.
+"""
+def test_is_vertex_cover_correct() -> bool:
+    # I have to build a bunch of graphs for testing the function
+    graph1 = Graph()
+    output1 = True
+    a = graph1.insert_vertex(True)
+    b = graph1.insert_vertex(True)
+    c = graph1.insert_vertex(False)
+
+    graph1.insert_edge(a, b)
+    graph1.insert_edge(a, c)
+    graph1.insert_edge(b, c)
+
+    # graph1.dump()
+
+    graph2 = Graph()
+    output2 = False
+    a = graph2.insert_vertex(True)
+    b = graph2.insert_vertex(False)
+    c = graph2.insert_vertex(False)
+
+    graph2.insert_edge(a, b)
+    graph2.insert_edge(a, c)
+    graph2.insert_edge(b, c)
+
+    # graph2.dump()
+
+    if is_vertex_cover_correct(graph1) != output1 or is_vertex_cover_correct(graph2) != output2:
+        return False
+    else:
+        return True
+
+
+# test is_vertex_cover_correct function
+print(test_is_vertex_cover_correct())
+
+# test build_random_graph function
+graph = build_random_graph(1000, 0.5)
+graph.dump()
---
+++ Problem5/test_functions.py
@@ -0,0 +1,54 @@
+from graph import Graph
+import random
+
+"""
+This module contains functions used for testing the solution of the vertex cover function
+"""
+
+"""
+It returns True if the vertex cover is evaluated correctly on the graph passed as parameter.
+
+It returns True if, for every pair of vertices connected by an edge, at least one of them is in the vertex cover.
+
+:param graph:   the graph on which the function evaluates if the vertex cover is evaluated correctly or not.
+
+:return         True if the vertex cover is evaluated correctly.
+                False otherwise.
+"""
+def is_vertex_cover_correct(graph: Graph) -> bool:
+    for edge in graph.edges():
+        (u, v) = edge.endpoints()
+        # if none of the two vertices is in the vertex cover, then the vertex cover is not evaluated correctly
+        if not (u.element() or v.element()):
+            return False
+
+    return True
+
+
+"""
+It builds a graph in a random way
+
+It builds a graph with the number of vertices passed as parameter.
+The edges are chosen randomly using the probability passed as parameter.
+The graph returned is not necessarily connected.
+
+:param number_vertices:     it is the number of vertices to add to the graph
+:param take_edge_prob:      it is the probability of taking an edge between two vertices
+
+:return                     A graph
+"""
+def build_random_graph(number_vertices: int = 100, take_edge_prob: float = 0.5) -> Graph:
+    graph = Graph()
+    vertices = []
+    # I add number_vertices vertices to the graph
+    for i in range(number_vertices):
+        vertices.append(graph.insert_vertex())
+
+    # for each vertex, for each possible edge, I decide if to add it or not
+    for i in range(number_vertices):
+        for j in range(i + 1, number_vertices):
+            choice = random.random()
+            if choice <= take_edge_prob:
+                graph.insert_edge(vertices[i], vertices[j])
+
+    return graph
