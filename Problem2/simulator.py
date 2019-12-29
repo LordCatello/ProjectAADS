979e8e63faadd271821b74413d110b3c2e72f5d9
Davide Cafaro <dvdcafaro@gmail.com>
2019-12-29 16:15:39 +0100

    Polished input validation and solved a bug.

--- Problem2/simulator.py
+++ Problem2/simulator.py
@@ -1,16 +1,14 @@
+from typing import Callable
+
 import Problem2
+from Problem2.schedulers.base_job_scheduler import BaseJobScheduler
+
+PROMPT_SYMBOL = "> "
 
 
 def main():
-    aging_interval_choice_prompt = "Insert the amount of time slices after which a job waiting in the queue will have " \
-                                   "" \
-                                   "" \
-                                   "" \
-                                   "" \
-                                   "" \
-                                   "" \
-                                   "its priority updated.\n" \
-                                   "> "
+    aging_interval_choice_prompt = "Insert the amount of time slices after which a job waiting in the queue will" \
+                                   " have its priority updated.\n" + PROMPT_SYMBOL
     aging_interval = choose_positive_integer(aging_interval_choice_prompt)
     scheduler_choice = choose_scheduler()
 
@@ -22,11 +20,12 @@ def main():
     job_in_execution = None
     while True:
         if job_in_execution is not None:
-            if job_in_execution.length > 0:
+            if job_in_execution.length > 1:
                 job_in_execution.length -= 1
                 print("Executing job {}. Time slices remaining: {}".format(job_in_execution.name,
                                                                            job_in_execution.length))
             else:
+                print("Finishing job {}. No time slices remaining.".format(job_in_execution.name))
                 job_in_execution = None
 
         add_job_choice = choose_add_job()
@@ -45,12 +44,9 @@ def ask_job() -> (str, int, int):
     It then returns this information.
     The function loops until it gets valid input."""
 
-    job_name = input("Insert the name for this job:\n"
-                     "> ")
-    job_priority = choose_positive_integer("Insert the priority for this job:\n"
-                                           "> ")
-    job_length = choose_positive_integer("Insert the length for this job:\n"
-                                         "> ")
+    job_name = input("Insert the name for this job:\n" + PROMPT_SYMBOL)
+    job_priority = choose_priority()
+    job_length = choose_length()
 
     return job_name, job_priority, job_length
 
@@ -59,7 +55,7 @@ def choose_add_job() -> str:
     """Returns 'y' or 'n', if the user wants to add or not a job.
     The function loops until it gets valid input."""
 
-    add_job_prompt = "Do you want to add a new job this time slice? (y/n)"
+    add_job_prompt = "Do you want to add a new job this time slice? (y/n)\n" + PROMPT_SYMBOL
     error_string = "Insert only 'y' or 'n'.\n"
     user_input = input(add_job_prompt)
     while user_input not in ('y', 'n'):
@@ -69,11 +65,47 @@ def choose_add_job() -> str:
     return user_input
 
 
-def choose_positive_integer(prompt: str) -> int:
-    """Returns a positive integer given by the user after a prompt.
+def choose_priority() -> int:
+    """Returns a valid priority number given by the user.
+    The function loops until it gets valid input."""
+    max_priority = BaseJobScheduler.Job.MAX_PRIORITY
+    min_priority = BaseJobScheduler.Job.MIN_PRIORITY
+    priority_error_string = "Insert a number between {} and {}.".format(max_priority, min_priority)
+    priority_check = lambda integer: max_priority <= integer <= min_priority
+
+    return choose_integer("Insert the priority for this job:\n"
+                          + PROMPT_SYMBOL, priority_error_string, priority_check)
+
+
+def choose_length() -> int:
+    """Returns a valid length number given by the user.
     The function loops until it gets valid input."""
 
-    error_string = "ERROR! Insert a valid positive number.\n"
+    min_length = BaseJobScheduler.Job.MIN_LENGTH
+    max_length = BaseJobScheduler.Job.MAX_LENGTH
+    length_error_string = "Insert a number between {} and {}.".format(min_length, max_length)
+    length_check = lambda integer: min_length <= integer <= max_length
+
+    return choose_integer("Insert the length for this job:\n"
+                          + PROMPT_SYMBOL, length_error_string, length_check)
+
+
+def choose_positive_integer(prompt: str) -> int:
+    """Returns a positive integer given by the user.
+    The function loops until it gets valid input."""
+
+    return choose_integer(prompt, "Please insert a positive integer.",
+                          lambda integer: integer > 0)
+
+
+def choose_integer(prompt: str, error_string: str, is_valid: Callable[[int], bool]) -> int:
+    """Returns an integer which respects a certain condition given by the user.
+    The function loops until it gets valid input.
+
+    :param prompt: The string to be displayed to the user to get the input.
+    :param error_string: The string to be displayed in case of invalid input.
+    :param is_valid: A callable object which takes an integer and returns a boolean value, used to validate the input.
+    """
 
     user_input = input(prompt)
     valid_input = True
@@ -81,7 +113,7 @@ def choose_positive_integer(prompt: str) -> int:
         user_input = int(user_input)
     except ValueError:
         valid_input = False
-    if valid_input and user_input < 1:
+    if valid_input and not is_valid(user_input):
         valid_input = False
 
     while not valid_input:
@@ -93,7 +125,7 @@ def choose_positive_integer(prompt: str) -> int:
         except ValueError:
             valid_input = False
             continue
-        if user_input < 1:
+        if not is_valid(user_input):
             valid_input = False
 
     return user_input
@@ -111,9 +143,7 @@ def choose_scheduler() -> int:
         Use a modulo job scheduler if you know that the aging interval of a job is very high compared to the average 
         length
         of the jobs, and if you know that the distribution in time of the jobs will be uniform.
-        Otherwise, a lazy job scheduler is usually the better choice.
-
-        > """
+        Otherwise, a lazy job scheduler is usually the better choice.\n\n""" + PROMPT_SYMBOL
     error_string = "ERROR! Please insert a 1 or a 2.\n"
 
     user_input = input(scheduler_choice_prompt)
