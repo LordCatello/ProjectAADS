7298358269536e758536b49ceffb467de37eac42
LordCatello <carmine_carratu@libero.it>
2020-01-03 21:43:07 +0100

    Started to implement the node and the tree

--- Problem1/BTree.py
+++ Problem1/BTree.py
@@ -1,24 +1,38 @@
 import numpy as np
+import platform
+from node import Node
 from collections import MutableMapping
-import sys
 
-BLOCK_SIZE = 1024
+BLOCK_DIM = 1024
+UINT = np.uint32
+POINTER_DIM = int(platform.architecture()[0][:2]) // 8
 
 class BTree(MutableMapping):
+    __slots__ = "_root", "_size", "_key_type", "_value_type", "_order", "_min_internal_num_children"
 
+    def __init__(self, key_type, value_type):
+        """
+        size:   Number of elements
 
+        """
 
-    def __init__(self):
+        self._root = None
+        self._size = 0
+        self._key_type = key_type
+        self._value_type = value_type
 
+        # note that an internal node
+        self._order = self._compute_order()
+        self._min_internal_num_children = self._order // 2
 
-    def __delitem__(self, v):
+    def __delitem__(self, key):
         pass
 
-    def __getitem__(self, k):
+    def __getitem__(self, key):
         pass
 
     def __len__(self) -> int:
-        pass
+        return self._size
 
     def __iter__(self):
         pass
@@ -26,11 +40,36 @@ class BTree(MutableMapping):
     def __setitem__(self, k, v) -> None:
         pass
 
-    def search_item(self):
-        pass
+    def _compute_order(self) -> int:
+        """
+        Evaluates the maximum number of children (d) allowed for the node.
 
+        :return:            Returns the maximum number of children allowed for the node.
+        """
+
+        pair_type = np.dtype([("key", self._key_type), ("value", self._value_type)])
+
+        size_dim = UINT(0).itemsize
+        node_dim = POINTER_DIM
+        pair_dim = pair_type.itemsize
+
+        # block_dim - numpy_array_overhead - size_dim - parent_dim (node_dim)
+        remaining_dim = BLOCK_DIM - size_dim - node_dim
+
+        order = (remaining_dim + pair_dim) // (pair_dim + node_dim)
+        return order
+
+    """
     def _add_root(self, k, v) -> _Node:
         list = list[self.order_d*None]
         list = np.ascontiguousarray(list)
         root = _Node.__init__(list[self.order_d*None], None)
+    """
 
+tree = BTree(np.dtype('U16'), int)
+
+pair_type = np.dtype([("key", tree._key_type), ("value", tree._value_type)])
+
+element1 = Node(pair_type, tree._order)
+
+print(element1._struct.itemsize)
--- Problem1/node.py
+++ Problem1/node.py
@@ -1,49 +1,126 @@
 import numpy as np
+import platform
 from sys import getsizeof
 
 UINT = np.uint32
-ARRAY_OVERHEAD = 48  # bytes
-WORD_SIZE = 8  # 64 bit
+POINTER_SIZE = int(platform.architecture()[0][:2]) // 8
 
 class Node:
-    def __init__(self, key_dtype, value_dtype, block_dim):
-        self._size = UINT(0)  # actual number of elements in the node
+    def __init__(self, pair_type, max_number_of_children, parent = None):
+        """
+        Note:
+        If you want to use a string, you have to use a dtype, str does not work, bceause you have to specify the dim of the str
+        for example you can specify np.dtype('U16') for a 16 char string.
+        In general, If you want to use an object, you have to define a dtype object, for the same reason of the str
+        (Node need to know the dimension of the memory to allocate).
 
-        pair_type = np.dtype([("key", key_dtype), ("value", value_dtype)])
+        """
 
-        # print(pair_type.itemsize)
-        order = Node._compute_order(pair_type, block_dim)
-        print(order)
-        # self._elements = np.dtype(pair_type, (order - 1,))
-        self._elements = np.empty(shape=order-1, dtype=pair_type)
-        self._children = np.empty(shape=order, dtype=np.int64)
-        print(self._elements)
-        # print(self._elements.itemsize)
-        # self._elements = np.ascontiguousarray([(3.6, 5.3), (6.4, 6.7)], pair_type)
+        # defines the "node" type
+        # I have to do this If I want that all the node is stored in a contiguous block of memory
+        node_type = np.dtype([("size", UINT), ("elements", pair_type, max_number_of_children - 1), ("children", Node, max_number_of_children), ("parent", Node)])
 
-    @staticmethod
-    def _compute_order(pair_type, block_dim) -> int:
-        int_size = getsizeof(UINT(0))
-        parent_size = getsizeof(np.int64(0))
+        # It creates a contiguous memory block (an array). The array is of one element, because we have
+        # previously defined a dtype that is a struct, containing the size, the elements (array), the children (array), the parent.
+        self._struct = np.empty(shape=1, dtype=node_type)
 
-        pointer_node_size = WORD_SIZE
-        pair_element_size = pair_type.itemsize
-        # we declares 2 array, one for the pair [key, value], and one for the children
-        arrays_overhead_size = 2 * ARRAY_OVERHEAD
+        # size is the number of elements in the 'elements' array
+        self._struct[0]['size'] = 0
 
-        # block_dim - dim(size) - dim(parent) - overhead(arrays)
-        remaining_dim = block_dim - int_size - parent_size - arrays_overhead_size
-        # print(remaining_dim)
+        # 'parent' should be None because np.empty should return already None
 
-        # This way I waste a pair
-        return remaining_dim // (pair_element_size + pointer_node_size)
+    @property
+    def size(self) -> UINT:
+        return self._struct[0]["size"]
+
+    @size.setter
+    def size(self, value: UINT):
+        self._struct[0]["size"] = value
+
+    @property
+    def parent(self) -> "Node":
+        return self._struct[0]["parent"]
+
+    @parent.setter
+    def parent(self, value: "Node"):
+        self._struct[0]["parent"] = value
+
+    def is_full(self) -> bool:
+        return self.size == len(self._struct[0]["elements"])
+
+    def is_empty(self) -> bool:
+        return self.size == 0
+
+    def add_element(self, key, value):
+        """
+        Adds an element (pair key,value), to the node.
+        If key is already present the new value is stored.
+        Takes O(n) because the elements are ordered.
+
+        :param key:         The key of the element
+        :param value:       The value of the element
+
+        :raise IndexError   Raise an exception if the node if full
+        """
+
+        if self.is_full():
+            raise IndexError
+
+        # Insert an element in an ordered array
+        # Search the position where to insert the element
+        # index = np.searchsorted(self._struct[0]["elements"], key)
+        # It's better to use a binary search
+        index = self.find_element_index(key)
+
+        # If the key is not present, I have to shift all the elements
+        if self._struct[0]["elements"][index]["key"] != key:
+            # Move all the elements greater than key and add the element to the index position.
+            for i in range(index, self.size):
+                self._struct[0]["elements"][self.size + index - i] = self._struct[0]["elements"][self.size + index - i - 1]
+            self.size = self.size + 1
+
+        # Insert the element in index
+        self._struct[0]["elements"][index] = (key, value)
+
+
+    def find_element_index(self, key) -> int:
+        # we have to implement a binary search, not a linear search
+        found = False
+        i = 0
+        index = None
+
+        while not found:
+            if i == self.size or self._struct[0]["elements"][i]["key"] >= key:
+                index = i
+                found = True
+
+            i += 1
+
+        return index
+
+    def get_element(self):
+        pass
+
+
 
 
 # peppe = Node(np.dtype('U16'), np.dtype(np.int32), 1024)
-element = Node(np.dtype(np.float64), np.dtype(np.float64), 1024)
+# element1 = Node(np.dtype(np.int64), np.dtype(np.int64), 256)
+pair_type = np.dtype([("key", np.dtype('U16')), ("value", int)])
+
+element1 = Node(pair_type, 5)
+
+element1.add_element("carmine", 3)
+element1.add_element("pippo", 3)
+element1.add_element("zelda", 5)
+element1.add_element("camillo", 9)
+
+print(element1._struct)
 
 """
-print(np.dtype(np.int32).itemsize)
-print(np.dtype(np.int16).itemsize)
-print(getsizeof(element._elements))
+print(element1._struct.itemsize)
+print(element1._struct[0]["size"].itemsize)
+print(element1._struct[0]["elements"].itemsize)
+print(element1._struct[0]["children"].itemsize)
+# print(element1._struct[0]["parent"].itemsize)
 """
