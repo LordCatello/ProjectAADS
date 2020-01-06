e32914a506146f6e171e8e1f471944cd99b2f53d
Davide Cafaro <dvdcafaro@gmail.com>
2020-01-06 17:09:27 +0100

    Created new test cases for all delete cases and organized it.

--- Problem1/test.py
+++ Problem1/test.py
@@ -9,19 +9,82 @@ from test_functions import random_int
 Test file
 
 """
-tree = BTree(int, int)
 
-for i in range(21):
-    tree[i]=i+1
+STANDARD_DIMENSION = 21
 
-print(check_tree(tree))
-print(tree.order)
-print(tree.min_internal_num_children)
-tree.dump_level()
 
-del tree[12]
-del tree[13]
+def set_up(dim):
+    tree = create_tree(dim)
+    print("start tree:")
+    tree.dump_level()
+    return tree
 
-tree.dump_level()
 
+def tear_down(tree):
+    if check_tree(tree):
+        print("The tree is a correct B-Tree.")
+    else:
+        print("The tree is NOT a correct B-Tree!")
+
+
+def delete_and_print_list(tree, key_list):
+    for key in key_list:
+        delete_and_print(tree, key)
+
+
+def delete_and_print(tree, key):
+    del tree[key]
+    print("Deleted {}:".format(key))
+    tree.dump_level()
+
+
+def create_tree(dim):
+    tree = BTree(int, int)
+
+    for i in range(dim):
+        tree[i] = i + 1
+
+    print(check_tree(tree))
+    print(tree.order)
+    print(tree.min_internal_num_children)
+
+    return tree
+
+
+def delete_transfer_right_case():
+    tree = set_up(STANDARD_DIMENSION)
+    delete_and_print_list(tree, [0, 1])
+    tear_down(tree)
+
+
+def delete_transfer_left_case():
+    # il transfer si genera indirettamente per uno swap del successore che genera underflow
+    tree = set_up(STANDARD_DIMENSION)
+    delete_and_print_list(tree, [0, 11, 12])
+    tear_down(tree)
+
+
+def delete_fusion_right_case():
+    tree = set_up(STANDARD_DIMENSION)
+    delete_and_print_list(tree, [0, 3, 1])
+    tear_down(tree)
+
+
+def delete_fusion_left_case():
+    tree = set_up(STANDARD_DIMENSION)
+    delete_and_print_list(tree, [3, 6, 7])
+    tear_down(tree)
+
+
+def delete_fusion_new_root():
+    tree = set_up(4)
+    delete_and_print_list(tree, [1, 3])
+    tear_down(tree)
+
+
+delete_fusion_new_root()
+delete_fusion_right_case()
+delete_transfer_right_case()
+delete_fusion_left_case()
+delete_transfer_left_case()
 
